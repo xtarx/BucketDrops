cc6100f170ba04590e2ac27d43c6a1481bd4d052
kimoktm <cg.karimtarek@gmail.com>
2016-03-29 17:29:31 +0200

    .Dialog Add Fragment .Dismiss Button

--- app/src/main/java/com/example/karim/bucketdrops/ActivityMain.java
+++ app/src/main/java/com/example/karim/bucketdrops/ActivityMain.java
@@ -3,18 +3,43 @@ package com.example.karim.bucketdrops;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
 
 public class ActivityMain extends AppCompatActivity {
+    private static final String TAG = "MyActivity";
+
     Toolbar mToolbar;
+    Button mBtnAdd;
+
+    private View.OnClickListener mBtnAddListener = new View.OnClickListener() {
+
+        @Override
+        public void onClick(View v) {
+            showDialogAdd();
+        }
+    };
+
+    private void showDialogAdd() {
+        Log.d(TAG, "showDialogAdd: Clicked");
+        DialogAdd dialog= new DialogAdd();
+        dialog.show(getSupportFragmentManager(),"Add");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        mBtnAdd = (Button) findViewById(R.id.btn_add);
+
+
+        mBtnAdd.setOnClickListener(mBtnAddListener);
+
         setSupportActionBar(mToolbar);
         initBackgroundImage();
     }
